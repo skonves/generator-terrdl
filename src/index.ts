c38cc96e9f44ecb635d4ad12264da69b6f38e944
Steve Konves <stephen.konves@gmail.com>
2018-08-08 23:00:23 -0700

    chore: scaffold

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export function reverseString(forward: string): string {
+  if (!forward) return forward;
+
+  return forward
+    .split('')
+    .reverse()
+    .join('');
+}
+
+console.log('This is the example typescript application!');
